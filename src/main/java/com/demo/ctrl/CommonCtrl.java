09906251a6808c0b789f307ac740f4e3fbe54622
陈大亮 <piaoyaocdl@163.com>
2017-08-18 09:33:12 +0800

    测试

--- src/main/java/com/demo/ctrl/CommonCtrl.java
+++ src/main/java/com/demo/ctrl/CommonCtrl.java
@@ -6,7 +6,6 @@ import lombok.extern.log4j.Log4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import sun.org.mozilla.javascript.internal.ast.ReturnStatement;
 
 import javax.servlet.http.HttpSession;
 
